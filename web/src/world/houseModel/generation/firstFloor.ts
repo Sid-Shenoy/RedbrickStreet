7210cb8090dd6b1c02a6fb4a003feb1e14c7859d
Sid Shenoy <sidharth.d.shenoy@gmail.com>
2025-12-26 16:15:22 -0500

    Improve efficiency of file dumper

--- web/src/world/houseModel/generation/firstFloor.ts
+++ web/src/world/houseModel/generation/firstFloor.ts
@@ -2,6 +2,7 @@ import type { HouseConfig } from "../../../types/config";
 import type { FloorModel } from "../types";
 import type { HouseGenContext } from "./context";
 
+// This is a stub
 export function generateFirstFloorModel(_house: HouseConfig, _ctx: HouseGenContext, _plot: FloorModel): FloorModel {
   return { regions: [], construction: [], objects: [] };
 }
--- web/src/world/houseModel/generation/secondFloor.ts
+++ web/src/world/houseModel/generation/secondFloor.ts
@@ -2,6 +2,7 @@ import type { HouseConfig } from "../../../types/config";
 import type { FloorModel } from "../types";
 import type { HouseGenContext } from "./context";
 
+// This is a stub
 export function generateSecondFloorModel(
   _house: HouseConfig,
   _ctx: HouseGenContext,
